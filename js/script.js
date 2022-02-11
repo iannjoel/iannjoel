a587db7c04b7a388a8a3edcce70c47eca262e015
iannjoel <ifernan1@binghamton.edu>
2022-02-11 06:49:15 -0500

    new
    hello

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-$(document).ready (function(){
-	$(window).scroll (function(){
+$(document).ready(function(){
+	$(window).scroll(function(){
 		if(this.scrollY > 20){
 			$('.navbar').addClass("sticky");
 		}else{
-			$('.navbar').removeClass(".sticky");
+			$('.navbar').removeClass("sticky");
 	}
 
 	});
 
 	//toggle menu/navbar script
-	$('.menu-btn').click(function(){
-		$('.navbar .menu').toggleClass("active");
-		$('.menu-btn i').toggleClass("active");
-	}); 
+	 $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
